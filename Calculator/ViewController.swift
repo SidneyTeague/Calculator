6f38f5642600c3b043cc689913d30a1f1cd39885
SidneyTeague <97697893+SidneyTeague@users.noreply.github.com>
2023-06-04 15:51:56 -0400

    Add files via upload

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Sidney Teague on 5/30/23.
+//	CPSC 5330 Assignment 2
+//
+//  Running on Xcode 12.4 and testing on an iPhone 12 Pro
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
